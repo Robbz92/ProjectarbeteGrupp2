64cbe5587423d2bc19dbc2700d4e400f3943d800
Anna Admeus <anna.admeus@hotmail.com>
2020-11-09 09:21:17 +0100

    Hantering bilder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,15 +2,20 @@ package com.company;
 
 import express.Express;
 import express.middleware.Middleware;
+import org.apache.commons.fileupload.FileItem;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
+
+
 
     public static void main(String[] args) {
         Database db = new Database();
         Express app = new Express();
+
 
         // add todoo items in sql
         app.post("/rest/notes", (req,res) ->{
@@ -26,6 +31,49 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+//Add pictures
+        app.get("/rest/posts", (req, res) -> {
+            List<postedPictures> posts = db.getPosts();
+            res.json(posts);
+        });
+
+        app.get("/rest/posts/:id", (req, res) -> {
+            int id = Integer.parseInt(req.getParam("id"));
+
+            postedPictures post = db.getPostById(id);
+            res.json(post);
+        });
+
+        app.post("/rest/posts", (req, res) -> {
+            postedPictures post = (postedPictures) req.getBody(postedPictures.class);
+
+            db.createPost(post);
+            res.send("post OK");
+        });
+
+        app.post("/api/file-upload", (req, res) -> {
+            String imageUrl = null;
+
+            // extract the file from the FormData
+            try {
+                List<FileItem> files = req.getFormData("files");
+                imageUrl = db.uploadImage(files.get(0));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            // return "/uploads/image-name.jpg
+            res.send(imageUrl);
+        });
+
+        // will serve both the html/css/js files and the uploads folder
+        try {
+            app.use(Middleware.statics(Paths.get("www").toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
 
         // Port server is listening on
         app.listen(3000);
---
+++ src/com/company/postedPictures.java
@@ -0,0 +1,82 @@
+package com.company;
+
+public class postedPictures {
+    private int id;
+    private String title;
+    private String content;
+    private String imageUrl;
+    private long dateTime;
+    private int category;
+
+public postedPictures(){}
+
+    public postedPictures(int id, String title, String content, String imageUrl, long dateTime, int category) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.imageUrl = imageUrl;
+        this.dateTime = dateTime;
+        this.category = category;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public long getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(long dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "postedPictures{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", dateTime=" + dateTime +
+                ", category=" + category +
+                '}';
+    }
+}
